5d4c508c3599cf754357014fe693cb0be1e4958d
Russ Hyde <russ@jumpingrivers.com>
2023-08-22 15:59:29 +0100

    fix: admissions_funnel_df must be non-reactive because it gets updated

--- R/app_server.R
+++ R/app_server.R
@@ -51,13 +51,11 @@ app_server <- function(input, output, session) {
   # Hence, the page is being hidden until it is required in the app.
   # When finalized a pin should be added for the admissions funnel data.
   if (get_golem_config("show_sunburst")) {
-    admissions_funnel_df <- shiny::reactive(
-      get_admissions_funnel(method = "from_sql")
-    )
+    admissions_funnel_df <- get_admissions_funnel(method = "from_sql")
 
     mod_sunburst_diagram_server(
       "admissions_funnel_sunburst_diagram",
-      df = admissions_funnel_df(),
+      df = admissions_funnel_df,
       step_cols = c("prospect_status", "admit_status")
     )
   }
